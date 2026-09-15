e8394d47cb9c26ff657bef550553005222234626
sleepsz <davidsonmichaud93@gmail.com>
2026-09-15 13:23:19 -0400

    Add files via upload

---
+++ P1LAB1_MichaudDavidson.py
@@ -0,0 +1,9 @@
+# Davidson Michaud
+# 9/10/2026
+# P1LAB1
+# This project will accept input and print out the input
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
